0c1da8bdb57b43e630ca14d21f5dda1afc8d121f
Matthew Ward <mattrward1030@gmail.com>
2021-03-14 11:08:43 -0400

    started function for saveBtn in with jquery

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,10 @@
-// current day 
+// current day using moment.js 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+$(".saveBtn").on("click", function () {
+
+})
+
+
+
