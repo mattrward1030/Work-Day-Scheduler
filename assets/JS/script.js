742e958e52c447014e48b48844ee50f8af04048f
Matthew Ward <mattrward1030@gmail.com>
2021-03-14 11:40:08 -0400

    for loop (each in jquery) to check the hour of the .time-block, will use it to compare against the currentHour

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,13 +2,22 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+
+// on click function of save button to save content to local storage
 $(".saveBtn").on("click", function () {
 
 })
 
 
-
+// function to checkhour against user selectedhour
 function checkHour() {
     var currentHour = moment().hour();
 
+    // loop for .time-block
+    $(".time-block").each(function () {
+        var selectedHour = parseInt($(this).attr("id"));
+        console.log(selectedHour, currentHour)
+    })
 }
+
+checkHour()
